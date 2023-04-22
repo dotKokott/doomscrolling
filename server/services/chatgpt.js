6b62830a9d44671462032ea110393ced19ba29d5
Christian Kokott <christian.kokott@gmail.com>
2023-04-22 12:44:37 +0200

    we got caching of prompts

--- server/services/chatgpt.js
+++ server/services/chatgpt.js
@@ -1,22 +1,51 @@
 import { Configuration, OpenAIApi} from 'openai'
+import fs from 'fs'
+import path from 'path'
 
 const systemPrompt = "You are a helpful assistant that follows the users instructions."
 
-export default {
-    openai: null,
+class ChatGPTService {
+    openai = null
+    promptCache = {}
+
+    constructor() {
+        this.getOpenAI()
+    }
+
+    async loadPromptCache() {
+        const promptCacheFileNamwe = path.join(__dirname, '../../promptCache.json')
+        if (fs.existsSync(promptCacheFileNamwe)) {
+            const data = fs.readFileSync(promptCacheFileNamwe)
+            this.promptCache = JSON.parse(data)
+        }
+
+        console.log(this.promptCache)
+    }
+
+    async savePromptCache() {
+        const promptCacheFileNamwe = path.join(__dirname, '../../promptCache.json')
+        fs.writeFileSync(promptCacheFileNamwe, JSON.stringify(this.promptCache))
+    }
 
     getOpenAI() {
         if (this.openai === null) {
             this.openai = new OpenAIApi(new Configuration({
                 apiKey: process.env.OPENAI_API_KEY
             }))
+
+            this.loadPromptCache()
         }
 
         return this.openai
-    },
-
+    }
 
     async getResponse(prompt, model = 'gpt-3.5-turbo', temperature = 0.7) {
+        // Check cache
+        console.log(this.promptCache)
+        if (this.promptCache[prompt] !== undefined) {
+            return this.promptCache[prompt]
+        }
+
         const response = await this.getOpenAI().createChatCompletion({
             model: model,
             "messages": [
@@ -25,6 +54,15 @@ export default {
             ],
         })
 
-        return response.data.choices[0].message.content
+
+        this.promptCache[prompt] = response.data.choices[0].message.content
+
+        this.savePromptCache()
+
+        return this.promptCache[prompt]
     }
 }
+
+const chatgptService = new ChatGPTService();
+
+export default chatgptService;
